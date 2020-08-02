12504f9005b727c4ebcca678ec05fb6132f89d0a
swdream <swdream.a@gmail.com>
2020-08-02 22:53:04 +0900

    [webi-5] test code modified

--- src/main/java/com/brwnsugr/webice/Application.java
+++ src/main/java/com/brwnsugr/webice/Application.java
@@ -1,9 +1,7 @@
 package com.brwnsugr.webice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Application { // Main Class of this project
     public static void main(String[] args){
